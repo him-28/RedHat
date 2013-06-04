7278f538b5aa99d4695dbbd29ee0294e1a13613a
Mac-8 <mac-8@Mac-8.local>
2013-06-04 15:04:09 +0800

    Initial Commit

---
+++ RedHat/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  RedHatAppController.h
+//  RedHat
+//
+//  Created by Mac-8 on 13/6/4.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ RedHat/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RedHatAppController.h
+//  RedHat
+//
+//  Created by Mac-8 on 13/6/4.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
